955c7b411bad8daf0f5d0e384189e0849882b3be
vnastoyashchy <vadim.89@ukr.net>
2020-01-19 12:54:22 +0200

    Add class MyMath Add program logic

---
+++ src/com/math/Main.java
@@ -0,0 +1,9 @@
+package com.math;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(MyMath.sum(2,3,4));
+        System.out.println(MyMath.subtraction(5,7,9));
+        System.out.println(MyMath.multiplication(1,4));
+    }
+}
---
+++ src/com/math/MyMath.java
@@ -0,0 +1,20 @@
+package com.math;
+
+public class MyMath {
+
+    public static final int a = 5;
+    public static final int b = 4;
+    public static final int c = 7;
+
+
+    public static int sum (int a, int b, int c){
+        return a + b + c;
+    }
+    public static int subtraction (int a, int b, int c){
+        return (a + b) - c;
+    }
+    public static int multiplication (int a, int b){
+        return a * b;
+    }
+
+}
